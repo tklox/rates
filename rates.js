35c03ceccb1b45be6b76be42089872be3d1e7429
tklox <128568749+tklox@users.noreply.github.com>
2023-03-22 16:31:12 +1100

    Update rates.js

--- rates.js
+++ rates.js
@@ -1,4 +1,4 @@
-fetch("https://cdn.jsdelivr.net/gh/tklox/rates@609730a61d9e12071ff00dae669441fa2a1b45a6/rates_header.php", {cache: "no-store"})
+fetch("https://github.com/tklox/rates/blob/56ae8f0434cd14a72772c814c94115a57350ac08/rates_header.php", {cache: "no-store"})
             .then(function (response) {
                 return response.json();
             })
