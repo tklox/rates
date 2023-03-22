56ae8f0434cd14a72772c814c94115a57350ac08
tklox <128568749+tklox@users.noreply.github.com>
2023-03-22 16:30:37 +1100

    Update rates_header.php

--- rates_header.php
+++ rates_header.php
@@ -1,3 +1,4 @@
+<?php
 header("Access-Control-Allow-Origin: *");
 header("content-type: application/json");
 echo file_get_contents("https://cdn.jsdelivr.net/gh/tklox/rates@ae77eab727fc00ed1ea6ac3b7f888b21fbd40130/rates.json");
