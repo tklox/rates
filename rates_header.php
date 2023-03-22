609730a61d9e12071ff00dae669441fa2a1b45a6
tklox <128568749+tklox@users.noreply.github.com>
2023-03-22 16:06:31 +1100

    Update rates_header.php

--- rates_header.php
+++ rates_header.php
@@ -1,3 +1,3 @@
 header("Access-Control-Allow-Origin: *");
 header("content-type: application/json");
-echo file_get_contents($_REQUEST['https://cdn.jsdelivr.net/gh/tklox/rates@ae77eab727fc00ed1ea6ac3b7f888b21fbd40130/rates.json'].".json");
+echo file_get_contents("https://cdn.jsdelivr.net/gh/tklox/rates@ae77eab727fc00ed1ea6ac3b7f888b21fbd40130/rates.json");
